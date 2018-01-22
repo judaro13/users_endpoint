a77d758414124b492231a4f1162ce073f0b3a992
Juliana Davila R <judaro13@gmail.com>
2018-01-22 09:22:47 -0500

    added rabbitmq publisher

--- main.go
+++ main.go
@@ -1,62 +1,69 @@
 package main
 
 import (
-	// "fmt"
 	"encoding/json"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/schema"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
-	"errors"
-)
 
+	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+	"github.com/judaro13/phoenix/publisher"
+)
 
 type User struct {
 	Name        string
-	Email 			string
-	Password  	string
+	Email       string
+	Password    string
 	Validated   bool
-	PhoneNumber   string
-	Country   	string
-	City   			string
-	Address   	string
+	PhoneNumber string
+	Country     string
+	City        string
+	Address     string
 }
 
 var (
-	ErrRequest = errors.New("request error")
-	ErrNameRequired = errors.New("the field 'name' is required")
-	ErrEmailRequired = errors.New("the field 'email' is required")
+	ErrRequest          = errors.New("request error")
+	ErrNameRequired     = errors.New("the field 'name' is required")
+	ErrEmailRequired    = errors.New("the field 'email' is required")
 	ErrPasswordRequired = errors.New("the field 'password' is required")
-	ErrPhoneRequired = errors.New("the field 'phoneNumber' is required")
+	ErrPhoneRequired    = errors.New("the field 'phoneNumber' is required")
 )
 
 func ValidateInputs(r *http.Request) error {
 	err := r.ParseForm()
 	if err != nil {
-    return ErrRequest
-  }
+		return ErrRequest
+	}
 
 	if len(r.FormValue("name")) == 0 {
 		return ErrNameRequired
 	}
-	if len(r.FormValue("email")) == 0  {
+	if len(r.FormValue("email")) == 0 {
 		return ErrEmailRequired
 	}
-	if len(r.FormValue("password")) == 0  {
+	if len(r.FormValue("password")) == 0 {
 		return ErrPasswordRequired
 	}
-	if len(r.FormValue("phoneNumber")) == 0  {
+	if len(r.FormValue("phoneNumber")) == 0 {
 		return ErrPhoneRequired
 	}
 	return nil
 }
 
-func CreateUserEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	err := ValidateInputs(r)
-	if err != nil{
-		w.WriteHeader(http.StatusBadRequest)
+func ReturnWithError(w http.ResponseWriter, err error, status int) bool {
+	if err != nil {
+		w.WriteHeader(status)
 		w.Write([]byte(err.Error()))
+		return true
+	}
+	return false
+}
+
+func CreateUserEndpoint(w http.ResponseWriter, r *http.Request) {
+	err := ValidateInputs(r)
+	if ReturnWithError(w, err, http.StatusBadRequest) {
 		return
 	}
 
@@ -64,25 +71,26 @@ func CreateUserEndpoint(w http.ResponseWriter, r *http.Request) {
 	user := new(User)
 	derr := decoder.Decode(user, r.PostForm)
 
-	if derr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(derr.Error()))
+	if ReturnWithError(w, derr, http.StatusInternalServerError) {
 		return
 	}
 
 	jsonUser, jerr := json.Marshal(user)
-  if jerr != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(jerr.Error()))
-    return
-  }
+	if ReturnWithError(w, jerr, http.StatusInternalServerError) {
+		return
+	}
 
+	fmt.Println("********************")
+	serr := publisher.SendMessage(string(jsonUser))
+	if ReturnWithError(w, serr, http.StatusInternalServerError) {
+		return
+	}
 
 	w.Write([]byte(jsonUser))
 }
 
-
 func main() {
+	fmt.Println("####################")
 	router := mux.NewRouter()
 	router.HandleFunc("/users", CreateUserEndpoint).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
